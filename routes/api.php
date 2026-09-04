3e3fe24b35ca06b3980762a626d3e69b63fbfbe3
Arleth Calizaya <helencalizaya03@gmail.com>
2026-09-03 23:00:40 -0400

    Prueba de creación de reporte exitosa usando Bearer Token

--- routes/api.php
+++ routes/api.php
@@ -13,3 +13,14 @@ use App\Http\Controllers\Api\AutenticacionController;
 
 Route::post('/registro', [AutenticacionController::class, 'registro']);
 Route::post('/login', [AutenticacionController::class, 'login']);
+
+
+
+use App\Http\Controllers\Api\ReporteController;
+
+// Estas rutas solo funcionan si el usuario envía su TOKEN
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/reportes', [ReporteController::class, 'crear']);
+    Route::get('/mis-reportes', [ReporteController::class, 'misReportes']);
+    Route::get('/reportes', [ReporteController::class, 'listarTodos']);
+});
