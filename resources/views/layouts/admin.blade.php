0f91fffa53dea98031c21d6012c82e279219eebe
Arleth Calizaya <helencalizaya03@gmail.com>
2026-09-03 23:35:27 -0400

    Prueba funcional: El administrador ya puede cambiar estados de reportes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -23,7 +23,15 @@
                 <a href="/admin/usuarios">👥 Usuarios</a>
                 <a href="/logout" class="text-danger">🚪 Cerrar Sesión</a>
             </nav>
+            
             <main class="col-md-10 p-4">
+                @if(session('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                   {{ session('success') }}
+                  <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                @endif
+
                 @yield('contenido')
             </main>
         </div>
